a03ef19d189df958673e3e0e57ea37e29786dbd5
Vinay Ram Gazula <vinayramgazula@gmail.com>
2024-11-02 05:52:04 -0400

    implemented custom dataset for RNN models; loaded into torch DataLoader successfully

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,3 +1,5 @@
+from torch.utils.data import DataLoader
+
 from tradeforecast.augmentation import DataEntryPoint, Indicators, FeatureEngg, RNNDataset
 
 #fpath = 'AAPL_1d_max_(None-None).csv'
@@ -12,7 +14,8 @@ indicators = Indicators(data_entry)
 indicators.add_macd_sl().add_rsi().add_atr()
 
 features = FeatureEngg(data_entry)
-features.add_quarters().add_weeks().add_hours()
+features.add_quarters().add_weeks()
+features.add_hours()
 
 print(data_entry.base_vars)
 print(data_entry.temporal)
@@ -24,14 +27,18 @@ lf = data_entry.data.drop_nulls()
 kwargs = {'lf': lf,
         'non_temporal': data_entry.non_temporal,
         'temporal': data_entry.temporal,
-        'target': ['Close'],
-        'seq_length': 30,
+        'target': ['Open','High'],
+        'seq_length': 5,
         'split': 0.2}
 
 train_dataset = RNNDataset(train=True, **kwargs)
 test_dataset = RNNDataset(train=False, **kwargs)
-#X, y = dataset[33]
-#print(X.shape,y)
+
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+print(train_loader.batch_size)
+print(train_loader.dataset)
 
 
 
--- tradeforecast/augmentation/datasets.py
+++ tradeforecast/augmentation/datasets.py
@@ -1,4 +1,6 @@
+from sklearn.utils.validation import check_is_fitted
 from sklearn.preprocessing import RobustScaler
+from sklearn.exceptions import NotFittedError
 from torch.utils.data import Dataset
 from torch import Tensor
 import polars as pl
@@ -26,34 +28,49 @@ class RNNDataset(DatasetBase):
         self.train_flag = train
         self.non_temporal = non_temporal
         self.temporal = temporal
+        self.features = self.non_temporal + self.temporal
         self.target = target
         self.seq_length = seq_length
         self.non_temporal_scaler = RobustScaler()
+        self.target_scaler = RobustScaler()
         self.tensors = self.__read_data__(split)
         pass
 
-    def __read_data__(self, split) -> tuple[Tensor, ...]:
-        lf_len = self.lf.select(pl.len()).collect().item()
-        slice_idx = int(math.ceil((1.0 - split)*lf_len))
-        self.lf = self.lf.slice(offset=0,length=slice_idx) if self.train_flag else self.lf.slice(offset=slice_idx,length=None)
-        pl_df_non_temporal = self.lf.select(self.non_temporal).collect()
-        self.non_temporal_scaler.set_output(transform='polars')
-        pl_df_non_temporal = self.non_temporal_scaler.fit_transform(pl_df_non_temporal)
-        #print(pl_df_non_temporal)
-        X = self.lf.select(self.non_temporal).collect().to_numpy()
-        y = self.lf.select(self.target).collect().to_numpy()
-        return (torch.from_numpy(X).to(torch.float32), torch.from_numpy(y).to(torch.float32))
-    
-    def __getitem__(self, index):
-        if index >= self.seq_length - 1:
-            index_start = index - self.seq_length + 1
-            x = self.X[index_start:(index + 1), :]
-        else:
-            padding = self.X[0].repeat(self.seq_length - index - 1, 1)
-            x = self.X[0:(index + 1), :]
-            x = torch.cat((padding, x), 0)
-        return x, self.y[index]
+    @property
+    def __lf_len__(self) -> int:
+        return self.lf.select(pl.len()).collect().item()
 
-    def __len__(self):
-        return self.X.shape[0]
+    def __read_data__(self, split) -> tuple[Tensor, ...]:
+        slice_idx = int(math.ceil((1.0 - split)*self.__lf_len__))
+        self.lf = self.lf.slice(offset=0,length=slice_idx) if self.train_flag else self.lf.slice(offset=slice_idx,length=None)
+        temporal_tensor: Tensor = self.lf.select(self.temporal).slice(0, self.__lf_len__ - 1).collect().to_torch(return_type='tensor', dtype=pl.Float32)
+        non_temporal_pl_df = self.lf.select(self.non_temporal).slice(0, self.__lf_len__ - 1).collect()
+        target_pl_df = self.lf.with_columns(pl.col(self.target).shift(n=-1)).select(self.target).drop_nulls().collect()
+        target_tensor: Tensor = self.fit_transform(target_pl_df, self.target_scaler)
+        non_temporal_tensor: Tensor = self.fit_transform(non_temporal_pl_df, self.non_temporal_scaler)
+        features_tensor = torch.cat((non_temporal_tensor, temporal_tensor), dim=1)
+        X = torch.empty((features_tensor.size(0) - self.seq_length + 1, self.seq_length, features_tensor.size(1)), dtype=torch.float32)
+        y = torch.empty((target_tensor.size(0) - self.seq_length + 1, self.seq_length, target_tensor.size(1)), dtype=torch.float32)
+        for i in range(X.size(0)):
+            for j in range(self.seq_length):
+                X[i][j][:] = features_tensor[i+j][:]
+                y[i][j][:] = target_tensor[i+j][:]
+        return (X, y)
     
+    def fit_transform(self, pl_df: pl.DataFrame, scaler: RobustScaler) -> Tensor:
+        try:
+            check_is_fitted(scaler)
+            raise ValueError(f"'{scaler}' is already fitted!")
+        except NotFittedError:
+            scaler.set_output(transform='polars')
+            pl_df: pl.DataFrame = scaler.fit_transform(pl_df)
+            return pl_df.to_torch(return_type='tensor', dtype=pl.Float32)
+    
+    def inverse_transform(self) -> Tensor:
+        return
+    
+    def __len__(self):
+        return self.tensors[0].size(0)
+    
+    def __getitem__(self, idx):
+        return tuple(tensor[idx] for tensor in self.tensors)
